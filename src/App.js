d18cc1c4d49a7f918b43fdb29b591ae5c9adfe0d
Luis Henrique Lipinski <lhlipinski@gmail.com>
2022-08-22 19:45:13 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as yup from "yup";
 
 function App() {
+
+  const handleClickLogin =(values) => console.log(values)
+  const validationLogin = yup.object().shape({
+    email: yup.string().email("Não é um email").required("Este Campo é Obrigatório"),
+    password: yup.string().min(8, "A senha deve ter no minimo 8 caracteres").required("Este campo é obrigatório"),
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Login</h1>
+      <Formik
+      initialvalues={{}}
+      onSubmit={handleClickLogin} 
+      validationSchema={validationLogin}
+      >
+        <Form className="login=form">
+          <div className="login-form-group">
+            <Field name="email" className="form-field" placeholder="Email"/>
+
+            <ErrorMessage component="span" name="email" className="form-error"/>
+          </div>
+
+          <div className="login-form-group">
+            <Field name="password" className="form-field" placeholder="Password"/>
+
+            <ErrorMessage component="span" name="password" className="form-error"/>
+          </div>
+          <button className="button" type="submit">Login</button>
+        </Form>
+        
+
+      </Formik>
     </div>
   );
 }
